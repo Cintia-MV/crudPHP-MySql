136fe67c2e303591d9b7d26735e5bce1ae31f200
Cintia Munoz Vales <cintiamuva@gmail.com>
2023-08-14 21:29:57 -0400

    CRUD PHP y MySql

---
+++ models/Categoria.php
@@ -0,0 +1,93 @@
+<?php
+
+class Categoria extends Conectar {
+
+    //Mostrar datos de la BD
+
+    public function get_categoria() {
+
+        $conectar = parent::conexion();
+
+        parent::set_name();
+
+        $sql = 'SELECT * FROM tm_categoria WHERE estado=1';
+        $sql = $conectar->prepare( $sql );
+        $sql->execute();
+
+        return $resultado = $sql->fetchAll( PDO::FETCH_ASSOC );
+
+    }
+
+    //Buscar por Id
+
+    public function get_categoriaxId( $cat_id ) {
+
+        $conectar = parent::conexion();
+
+        parent::set_name();
+
+        $sql = 'SELECT * FROM tm_categoria WHERE estado=1 AND cat_id=?';
+        $sql = $conectar->prepare( $sql );
+        $sql->bindValue( 1, $cat_id );
+        $sql->execute();
+
+        return $resultado = $sql->fetchAll( PDO::FETCH_ASSOC );
+
+    }
+
+    //Crear nuevo producto
+
+    public function insert_categoria( $cat_nombre, $cat_obs ) {
+
+        $conectar = parent::conexion();
+
+        parent::set_name();
+
+        $sql = "INSERT INTO tm_categoria(cat_id, cat_nombre, cat_obs, estado) VALUES(NULL, ?,?,'1')";
+        $sql = $conectar->prepare( $sql );
+        $sql->bindValue( 1, $cat_nombre );
+        $sql->bindValue( 2, $cat_obs );
+        $sql->execute();
+
+        return $resultado = $sql->fetchAll( PDO::FETCH_ASSOC );
+
+    }
+
+    //Actualizar producto
+
+    public function update_categoria( $cat_id, $cat_nombre, $cat_obs ) {
+
+        $conectar = parent::conexion();
+
+        parent::set_name();
+
+        $sql = 'UPDATE tm_categoria SET cat_nombre=?, cat_obs=? WHERE cat_id=?';
+        $sql = $conectar->prepare( $sql );
+        $sql->bindValue( 1, $cat_nombre );
+        $sql->bindValue( 2, $cat_obs );
+        $sql->bindValue( 3, $cat_id );
+        $sql->execute();
+
+        return $resultado = $sql->fetchAll( PDO::FETCH_ASSOC );
+
+    }
+
+    //Desactivar producto
+    public function delete_categoria( $cat_id ) {
+
+        $conectar = parent::conexion();
+
+        parent::set_name();
+
+        $sql = "UPDATE tm_categoria SET estado='0' WHERE cat_id=?";
+        $sql = $conectar->prepare( $sql );
+        $sql->bindValue( 1, $cat_id );
+        $sql->execute();
+
+        return $resultado = $sql->fetchAll( PDO::FETCH_ASSOC );
+
+    }
+
+}
+
+?>
